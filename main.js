cf8fed12105307c1a2f3549cc2a50faa0b3221f7
Debison Johan <debison.perez@cun.edu.co>
2023-02-25 09:19:48 -0500

    v2

---
+++ main.js
@@ -0,0 +1,11 @@
+var contador = 0;
+var myButton =document.getElementById("boton");
+var myText = document.getElementById("texto");
+
+myButton.onclick = function () {
+    contador++;
+    if (contador>9) {
+        contador = 0;
+    }
+    myText.textContent = contador;
+}
